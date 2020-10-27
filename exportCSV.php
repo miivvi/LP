2188276033d56acd83f4e38aa3e89c8c5f3f7a1a
miivvigit <miivvigit@gmail.com>
2020-10-28 02:12:56 +0300

    SCR #0 init

---
+++ exportCSV.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace csvProject;
+
+final class CSV {
+
+    public static function generate(){
+        return true;
+    }
+
+    public function defaultReport(){
+        return true;
+    }
+
+    public function header(){
+        return true;
+    }
+
+    public function content(){
+        return true;
+    }
+
+}
